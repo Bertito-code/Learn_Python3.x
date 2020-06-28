d09da1a7f984e1049361fede724eb982278c012b
Alberto González <gonzalezlafuentealberto@gmail.com>
2020-06-28 12:23:17 +0200

    Bucle While

---
+++ Codigo_como_Documentacion/5-bucle_while.py
@@ -0,0 +1,7 @@
+i = 0  # Valor de i inicial
+
+while i <= 3:  # Mientras i sea igual o menor de 3 seguira en el bucle
+    print(i)  # Imprime el valor de i
+    i += 1  # i = i + 1   Cada vez que pasa por aqui se le suma 1 a i
+else:
+    print("Fin bucle while")  # Cuando se sale del bucle
