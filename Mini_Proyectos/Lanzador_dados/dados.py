81284084b20b587e5f51876970090d8d64423962
Alberto González <gonzalezlafuentealberto@gmail.com>
2020-07-01 19:48:24 +0200

    Update dados.py

--- Mini_Proyectos/Lanzador_dados/dados.py
+++ Mini_Proyectos/Lanzador_dados/dados.py
@@ -54,49 +54,31 @@ def dx():
     print(aleatorio)
 
 # Programa
-print(" Bienvenido al lanzador de dados Version 0.1")
-print(" Si quieres lanzar dados pulsa 1, si quieres salir pulsa 2")
-numero_introducido = int(input())
-tipo_dado_in = int(input("Introduce el numero de caras que tiene el dado: "))
-numero_dados = int(input("Introduce el numero de dados que se lanzaran"))
+# Programa
+numero_introducido = 1
+print("Bienvenido al lanzador de dados")
+while numero_introducido == 1:
+    numero_introducido = int(input(" Si quieres lanzar dados pulsa 1, si quieres salir pulsa 2     "))
+    if numero_introducido == 1:
+        tipo_dado_in = int(input("Introduce el numero de caras que tiene el dado: "))
+        numero_dados = int(input("Introduce el numero de dados que se lanzaran"))
+        contador = numero_dados
+        while contador > 1:
+            contador = contador -1
+            if tipo_dado_in == 4:
+                d4()
+            elif tipo_dado_in == 6:
+                d6()
+            elif tipo_dado_in == 8:
+                d8()
+            elif tipo_dado_in == 12:
+                d12()
+            elif tipo_dado_in == 20:
+                d20()
+            elif tipo_dado_in == 100:
+                d100()
+        else:
+            dx()   
 
-contador = numero_dados
-while contador > 1:
-    contador = contador -1
-    if tipo_dado_in == 4:
-        d4()
-    elif tipo_dado_in == 6:
-        d6()
-    elif tipo_dado_in == 8:
-        d8()
-    elif tipo_dado_in == 12:
-        d12()
-    elif tipo_dado_in == 20:
-        d20()
-    elif tipo_dado_in == 100:
-        d100()
-else:
-    dx()   
-
-
-
-# PRUEBAS
-#print(randint(1,tipo_dado_in))
-#print(aleatorio)
-
-# Solo dados D20
-#if aleatorio == tipo_dado_in:
-#    print("CRITICO")
-#elif aleatorio == 1:
-#    print("PIFIA")
-#else:
-#    print(aleatorio)
-
-#d4()
-#d6()
-#d8()
-#d12()
-#d20()
-#d100()
 
 
