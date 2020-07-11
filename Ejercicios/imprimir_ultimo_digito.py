83052813dc9a7d39e6660ca65463c21f1e10fe72
Alberto González <gonzalezlafuentealberto@gmail.com>
2020-07-11 09:49:52 +0200

    imprimir ultimo digito numero

---
+++ Ejercicios/imprimir_ultimo_digito.py
@@ -0,0 +1,11 @@
+
+# Escribe un programa que introduciendo un numero entero, imprima el ultimo digito de ese numero.
+
+
+
+
+
+numero = input(" Introduce un numero entero:   ")
+ultimo_digito = int(numero[-1])
+print(ultimo_digito)
+
